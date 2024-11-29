4968c913ba791146f3eefd0fcc9fd9459b4e8f39
rhyf-ai <rhyf40617@gmail.com>
2024-11-29 18:17:50 +0900

    배경화면 css로 바꿈

--- sync/components/BodyContainer.jsx
+++ sync/components/BodyContainer.jsx
@@ -1,10 +1,81 @@
-'use client'
+"use client";
 
 import React from "react";
 import "./style/bodycontainer.css"; // CSS 파일 임포트
 import { useShowChatStore } from "@/app/stores/showChatStore";
+
 export default function BodyContainer({ children }) {
     const isMinimized = useShowChatStore((state) => state.isMinimized);
-    // return <div className={isMinimized ? 'styled-container' : 'styled-container flex justify-center items-center'}>{children}</div>;
-    return <div className={isMinimized ? 'styled-container-bg' : 'styled-container-bg flex justify-center items-center'}>{children}</div>;
+    return (
+        <div
+            className={
+                isMinimized
+                    ? "styled-container-bg relative w-screen h-screen overflow-hidden"
+                    : "styled-container-bg flex justify-center items-center relative w-screen h-screen overflow-hidden"
+            }
+        >
+            {/* 첫 번째 배경 */}
+            <div
+                style={{
+                    width: "100vw",
+                    height: "100vh",
+                    top: "0",
+                    left: "0",
+                    position: "absolute",
+                    background:
+                        "linear-gradient(180deg, #08001F 0%, #140043 48%, #0D0010 100%)",
+                }}
+            ></div>
+
+            {/* 두 번째 배경 */}
+            <div
+                style={{
+                    width: "48.59vw", // 933px / 1920px * 100vw
+                    height: "48.59vw",
+                    left: "59.01vw", // 1133px / 1920px * 100vw
+                    top: "4.81vh", // 52px / 1080px * 100vh
+                    position: "absolute",
+                    background:
+                        "radial-gradient(100% 100% at 50% 50%, rgba(0, 106, 255, 0.16) 0%, rgba(122, 98, 255, 0.16) 100%)",
+                    borderRadius: "9999px",
+                    boxShadow: "20.83vw 20.83vw 20.83vw rgba(0, 0, 0, 0)", // 400px / 1920px * 100vw
+                    filter: "blur(20.83vw)", // 400px / 1920px * 100vw
+                }}
+            ></div>
+
+            {/* 세 번째 배경 */}
+            <div
+                style={{
+                    width: "29.38vw", // 564px / 1920px * 100vw
+                    height: "29.38vw",
+                    left: "0",
+                    top: "68.15vh", // 736px / 1080px * 100vh
+                    position: "absolute",
+                    background:
+                        "radial-gradient(100% 100% at 50% 50%, rgba(0, 106, 255, 0.70) 0%, rgba(68, 38, 236, 0.70) 54%, rgba(165, 0, 255, 0) 100%)",
+                    borderRadius: "9999px",
+                    boxShadow: "31.25vw 31.25vw 31.25vw rgba(0, 0, 0, 0)", // 600px / 1920px * 100vw
+                    filter: "blur(31.25vw)", // 600px / 1920px * 100vw
+                }}
+            ></div>
+
+            {/* 네 번째 배경 */}
+            <div
+                style={{
+                    width: "29.06vw", // 558px / 1920px * 100vw
+                    height: "29.06vw",
+                    left: "27.71vw", // 532px / 1920px * 100vw
+                    top: "42.59vh", // 460px / 1080px * 100vh
+                    position: "absolute",
+                    background:
+                        "radial-gradient(100% 100% at 50% 50%, rgba(0, 106, 255, 0.20) 0%, rgba(122, 98, 255, 0.20) 100%)",
+                    borderRadius: "9999px",
+                    boxShadow: "20.83vw 20.83vw 20.83vw rgba(0, 0, 0, 0)", // 400px / 1920px * 100vw
+                    filter: "blur(20.83vw)", // 400px / 1920px * 100vw
+                }}
+            ></div>
+
+            {children}
+        </div>
+    );
 }
--- sync/components/Header.jsx
+++ sync/components/Header.jsx
@@ -1,41 +1,33 @@
-"use client";
-import styled from "styled-components";
-import { useState } from "react";
+'use client';
+import styled from 'styled-components';
+import { useState } from 'react';
 
 const HeaderContainer = styled.div``;
 
 const NavBorder = styled.div`
     position: absolute;
-    bottom: -17px;
+    bottom: -9.5px;
     left: 50%;
     transform: translateX(-50%);
     height: 2px;
     width: 80px;
     background-color: white;
-`
+`;
 
 const Header = () => {
     return (
-        <header className="fixed inset-x-0 top-0 z-50 left-0 w-full flex gap-32 justify-start p-8 pb-4 border-b border-purple-500 border-opacity-10">
-            <a href="/"> {/*Link to the home page 새로고침하고 싶으면 Link로 하면 됨. */}
-                <img
-                    className="h-20 md:h-12"
-                    src="/img/logo/Rhyf_logo.svg"
-                    alt=""
-                />
+        <header className="fixed inset-x-0 top-0 z-50 left-0 w-full flex gap-32 justify-start p-8 pb-2 border-b border-purple-500 border-opacity-10">
+            <a href="/ ">
+                {' '}
+                {/*Link to the home page 새로고침하고 싶으면 Link로 하면 됨. */}
+                <img className="ml-[3vw] mt-[1.5vh]" src="/img/logo/Rhyf_logo.svg" alt="" style={{ height: '4vh' }} />
             </a>
-            <div className="self-end flex gap-20 text-xl font-semibold">
-                <a
-                    href="/"
-                    className="relative"
-                >
+            <div className="self-end flex gap-20 text-l font-semibold mt-[2vh]">
+                <a href="/" className="relative ">
                     Create
                     <NavBorder />
                 </a>
-                <a
-                    href="/"
-                    
-                >
+                <a href="/" className="relative ">
                     History
                 </a>
             </div>
